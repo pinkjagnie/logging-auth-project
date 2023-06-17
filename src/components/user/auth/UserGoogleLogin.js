967e66a6f10c3ef1ddf2c02879a960bbf0b04d33
pinkjagnie <olga.krzysztofik@gmail.com>
2023-06-17 19:53:01 +0200

    Fixed logging with google account

--- src/components/user/auth/UserGoogleLogin.js
+++ src/components/user/auth/UserGoogleLogin.js
@@ -7,7 +7,7 @@ const UserGoogleLogin = () => {
 
   return(
     <section className='pt-4'>
-      <div className='w-[200px] flex justify-around items-center mx-auto px-10 py-4 bg-gray-500 text-zinc-200 cursor-pointer' onClick={() => signIn()}>
+      <div className='w-[200px] flex justify-around items-center mx-auto px-10 py-4 bg-gray-500 text-zinc-200 cursor-pointer' onClick={() => signIn('google')}>
         <BsGoogle size={30}/>
         <p className='uppercase text-lg'>Google</p>
       </div>
