5f02f1112e07a85bc1b7305c04772e4e1fbd4257
pinkjagnie <olga.krzysztofik@gmail.com>
2023-06-20 10:30:54 +0200

    Added simple subpage to confirm login with OTPcode

---
+++ src/pages/user/login/[slug].js
@@ -0,0 +1,76 @@
+import React from "react";
+
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+export default function Hash(props) {
+
+  const { register, handleSubmit, reset, formState: { errors, isDirty, isValid } } = useForm({
+    defaultValues: {
+      otpcode: ''
+    }
+  }); 
+
+  async function onSubmit(data) {
+    const enteredOtpCode = data.otpcode;
+
+    console.log(enteredOtpCode);
+
+    reset();
+  };
+
+  return(
+    <section className="pt-20">
+      <div className="w-[90%] md:w-[70%] mx-auto">
+        <h1 className="font-bold text-3xl uppercase text-center py-10">please enter below your secret code:</h1>
+        
+        <form className="w-[90%] md:w-[60%] p-6 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+
+          <div className="pb-6">
+            <label htmlFor='otpcode' className="pl-2">Enter your code *</label>
+            <input type='number' id='otpcode' className="w-[100%] border-b border-gray-900" {...      register("otpcode", { required: {
+            value: true,
+            message: 'This field is required'
+            }})} />
+            {errors.otpcode && <p className="text-pink-900 italic">{errors.otpcode.message}</p>}
+          </div>
+
+          <div className="flex justify-center py-4">
+            <button type="submit" className='w-[80%] outline px-6 py-4 font-medium bg-gray-700 disabled:bg-gray-500 text-zinc-200 hover:bg-gray-600' disabled={!isDirty || !isValid}>Confirm</button>
+          </div>
+        </form>
+      
+      </div>
+    </section>
+  )
+}
+
+// export async function getServerSideProps(context) {
+
+//   const { params } = context;
+//   const { slug } = params;
+
+//   const response = await axios.get(
+//     `http://localhost:3000/api/auth/confirm/${slug}`
+//   );
+//   let user = response.data[0]
+
+//   await axios.patch(`http://localhost:3000/api/auth/confirm/${slug}`, { active: true })
+  
+  // await fetch(`http://localhost:3000/api/auth/confirm/${slug}`, {
+  //   method: "PATCH",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     active: true,
+  //   })
+  // });
+  
+//    return {
+//     props: {
+//       user: user,
+//       slug: slug,
+//     }
+//   };
+// }
